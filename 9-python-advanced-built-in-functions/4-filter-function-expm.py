8e5c58195a4af61f36f1b1f8ef1b53962c7114a8
saidulislam <saidul.islam@gmail.com>
2021-09-19 18:00:10 -0400

    more examples on map and filter

--- 9-python-advanced-built-in-functions/4-filter-function-expm.py
+++ 9-python-advanced-built-in-functions/4-filter-function-expm.py
@@ -18,3 +18,37 @@ print(starts_with_a)
 # start_with_a can be a generator obj like the following
 starts_with_a = (a for a in friends if a.startswith('A'))
 print([f for f in starts_with_a])
+
+
+# Much like map, filter calls a function (known as a predicate) 
+# for every item in an iterable, and it discards any values for which that function returns a falsy value.
+
+# A predicate is a function that accepts some value as an argument and returns either True or False.
+
+numbers = [1, 56, 3, 5, 24, 19, 88, 37]
+even_numbers = filter(lambda number: number % 2 == 0, numbers)
+
+print(*even_numbers)
+
+# of course, you can also call a function in filter
+def is_even(number):
+	return number % 2 == 0
+
+numbers = [1, 56, 3, 5, 24, 19, 88, 37]
+even_numbers = filter(is_even, numbers)
+
+print(*even_numbers)
+
+# Using None with filter
+# Instead of passing in a function to filter, it's possible to use the value None. 
+# This tells filter that we want to use the truth values of the values directly, instead of performing 
+# some kind of comparison, or calculating something.
+
+# In this case, filter will keep all truthy values from the original iterable, and all falsy values 
+# will be discarded.
+
+values = [0, "Hello", [], {}, 435, -4.2, ""]
+truthy_values = filter(None, values)
+
+print(*truthy_values, sep=", ")  # Hello, 435, -4.2
+
--- 9-python-advanced-built-in-functions/5-map-function-expm.py
+++ 9-python-advanced-built-in-functions/5-map-function-expm.py
@@ -12,3 +12,88 @@ print([f for f in l_friends])
 # you can also do
 l_friends = [f.lower() for f in friends]
 print(l_friends)
+
+
+"""
+map is a function that allows us to call some other function on every item in an iterable. 
+And what are functions? They're just a means of defining some action we want to perform in a reusable way. 
+In other words, map is a way of performing some action for every item in an iterable, just like a comprehension.
+
+Let's say I want to cube every number in a list of numbers. We can use map like so:
+
+"""
+
+def cube(number):
+	return number ** 3
+
+numbers = [1, 2, 3, 4, 5]
+cubed_numbers = map(cube, numbers)
+
+for number in cubed_numbers:
+	print(number)
+
+
+""" 
+Since they're iterable, we can also unpack them using *
+"""
+
+def cube(number):
+	return number ** 3
+
+numbers = [1, 2, 3, 4, 5]
+cubed_numbers = map(cube, numbers)
+
+print(*cubed_numbers, sep=", ")
+
+""" We can also convert them to a normal collection if we like """
+def cube(number):
+	return number ** 3
+
+numbers = [1, 2, 3, 4, 5]
+cubed_numbers = list(map(cube, numbers))
+print(cubed_numbers)
+
+
+# map with multiple iterables
+# One of the really nice things about map is that is can handle several iterables at once.
+def add(a, b):
+	return a + b
+
+odds = [1, 3, 5, 7, 9]
+evens = [2, 4, 6, 8, 10]
+
+totals = map(add, odds, evens)
+print(*totals, sep=", ")  # 3, 7, 11, 15, 19
+
+
+# map with lambda expressions
+numbers = [1, 2, 3, 4, 5]
+cubed_numbers = map(lambda number: number ** 3, numbers)
+print(*cubed_numbers)
+
+
+# We could have used a lambda expression here like this:
+
+odds = [1, 3, 5, 7, 9]
+evens = [2, 4, 6, 8, 10]
+
+totals = map(lambda a, b: a + b, odds, evens)
+print(*totals, sep=", ")  # 3, 7, 11, 15, 19
+
+# btw... operator has an add function ready to go, so we don't have to define it ourselves.
+from operator import add
+
+odds = [1, 3, 5, 7, 9]
+evens = [2, 4, 6, 8, 10]
+
+totals = map(add, odds, evens)
+print(*totals, sep=", ")  # 3, 7, 11, 15, 19
+
+
+# Another really useful function in operator is methodcaller. methodcaller allows us to 
+# easily define a function that calls a method for us. We just have to provide the method name as a string.
+from operator import methodcaller
+
+names = ["tom", "richard", "harold"]
+title_names = map(methodcaller("title"), names)
+print(*title_names)
