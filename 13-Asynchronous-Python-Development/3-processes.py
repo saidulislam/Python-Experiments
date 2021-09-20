2369cf8f91737efa721e049f627f6638285f3059
saidulislam <saidul.islam@gmail.com>
2021-09-20 11:10:32 -0400

    processes experiment

---
+++ 13-Asynchronous-Python-Development/3-processes.py
@@ -0,0 +1,42 @@
+from multiprocessing import Process
+import time
+
+####### SINGLE PROCESS
+
+def ask_user():
+	start = time.time()
+	user_input = input('Enter your name: ')
+	greet = f'Hello, {user_input}'
+	print(greet)
+	print('ask_user: ', time.time() - start)
+
+def complex_calculation():
+	print('Started calculating...')
+	start = time.time()
+	[x**2 for x in range(20000000)]
+	print('complex_calculation: ', time.time() - start)
+
+
+# With a single thread, we can do one at a time—e.g.
+# start = time.time()
+# ask_user()
+# complex_calculation()
+# print('Single thread total time: ', time.time() - start, '\n\n')
+
+
+####### TWO PROCESSES
+
+
+# With two processes, we can do them both at once...
+process1 = Process(target=complex_calculation)
+process2 = Process(target=complex_calculation)
+process1.start()
+process2.start()
+
+start = time.time()
+
+process1.join()
+process2.join()
+
+print('Two process total time: ', time.time() - start)
+
