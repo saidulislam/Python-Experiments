b6def13c73dd4881ba0bc5def658cf3e1820d6cf
saidulislam <saidul.islam@gmail.com>
2021-09-22 17:34:51 -0400

    error page mapping

---
+++ 15-web-development-with-flask/4-error-pages/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+posts = {
+    0: {
+        'title': 'Hello World',
+        'content': 'This is my first blog post!'
+    }
+}
+
+
+@app.route("/")
+def home():
+    return "Hello World!"
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = posts.get(post_id)
+    # If the post was not found, then we can render 
+    # a different page instead—the user will see an error page.
+    if not post:
+        # Here we pass another variable, the string `message`.
+        # It can be used inside the jinja template.
+        return render_template('404.jinja2', message=(f'A post with id {post_id} was not found.'))
+    return render_template('post.jinja2', post=post)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
